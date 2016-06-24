5932adcfe439d9d184859a777717f10d703b9bbc
prokop <prokop@debiannb.paruzkovi.cz>
2016-06-24 20:20:18 +0200

    mujdruhycommit

--- sevry.c
+++ sevry.c
@@ -1,16 +1,15 @@
 //dodelat for zmenit
 #include<stdio.h>
 int severy[][2]{
-1,8,
-2,87,
-3,9
+	1,8,
+	2,87,
+	3,9
 };
 int sever, i;
-int main()
-{
-printf("zadejte cislo serveru\n");
-scanf("%d",&server);
-for(i=1;i<=3;i++){
+int main(){
+	printf("zadejte cislo serveru\n");
+	scanf("%d",&server);
+	for(i=1;i<=3;i++){
 	if(i==server)printf("je pripojeno %d uzivatelu\n",servery[i-1][]);
 }
 return 0;
