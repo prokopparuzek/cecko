78f101e4c08172c7002ed2350223c7be4b109966
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2016-06-24 21:08:13 +0200

    treticommit

--- sevry.c
+++ sevry.c
@@ -1,16 +1,20 @@
 //dodelat for zmenit
 #include<stdio.h>
-int severy[][2]{
-	1,8,
-	2,87,
-	3,9
+int servery[][2]={
+1,8,
+2,87,
+3,9
 };
-int sever, i;
+int server, i;
 int main(){
-	printf("zadejte cislo serveru\n");
-	scanf("%d",&server);
-	for(i=1;i<=3;i++){
-	if(i==server)printf("je pripojeno %d uzivatelu\n",servery[i-1][]);
+printf("zadejte cislo serveru\n");
+scanf("%d",&server);
+if(server>3){
+printf("ERROR\n");
+return 0;
+}
+for(i=1;i<=3;i++){
+	if(i==server)printf("je pripojeno %d uzivatelu\n",servery[i-1][1]);
 }
 return 0;
 }
