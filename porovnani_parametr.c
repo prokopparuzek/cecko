ab984310811cedd3c32e3ff2cfd41c824c06ae36
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2016-09-09 15:43:31 +0200

    12.

--- porovnani_parametr.c
+++ porovnani_parametr.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 int main(int argc, char *argv[])
 {
-char *a = argv[1],*b = argv[2];
-int test = strcmp(a,b);
-if(test == 0) printf("jsou stejne\n");
-else if(test > 0) printf("a > b\n");
-else if(test < 0) printf("a < b\n");
+if(argc == 3){
+	char *a = argv[1],*b = argv[2];
+	int test = strcmp(a,b);
+	if(test == 0) printf("jsou stejne\n");
+	else if(test > 0) printf("a > b\n");
+	else if(test < 0) printf("a < b\n");
+}
 return 0;
 }
