3d81d5c4800037681fb093b40d5d5ad36b1db554
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2017-11-10 15:47:39 +0100

    mycat2

--- soubory/mycat.c
+++ soubory/mycat.c
@@ -2,19 +2,22 @@
 #include<stdlib.h>
 int main(int argc, char *argv[])
 {
-int c;
+int c, i;
 FILE *fp;
-if (argc != 2)
+if (argc < 2)
 {
     puts("Chybny pocet argumentu!");
     exit(1);    
 }
-if ((fp = fopen(argv[1],"r")) == NULL)
+for (i = 1; i < argc; i++)
 {
-    printf("Nelze otevrit soubor %s pro cteni!\n",argv[1]);
-    exit(2);   
+    if ((fp = fopen(argv[i],"r")) == NULL)
+    {
+        printf("Nelze otevrit soubor %s pro cteni!\n",argv[i]);
+        exit(2);   
+    }
+    while ((c = fgetc(fp)) != EOF) putchar(c);
+    fclose(fp);
 }
-while ((c = fgetc(fp)) != EOF) putchar(c);
-fclose(fp);
 return 0;    
 }
