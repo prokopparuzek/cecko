ffed9cca46c4a24365249b0eab69de39dd82f1af
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2017-12-24 09:07:11 +0000

    bins

--- cviceni/opilpr.c
+++ cviceni/opilpr.c
@@ -20,14 +20,15 @@ cesta[max + 1] = 0;
 *(cesta + (k - 1)) = OPIL;
 srand((unsigned int) time(NULL));
 for (i = k - 1; i != 0 && i != max - 1;) {
+    puts(cesta);
     int r = 0;
     cesta[i] = CES;
     r = rand() % 100;
     if (r < p) i--;
     else i++;
     cesta[i] = OPIL;
-    puts(cesta);
 }
+puts(cesta);
 //if (i) exit(1);
 //else exit(0);
 if (i == 0) puts("Hospoda");
--- cviceni/pascal.c
+++ cviceni/pascal.c
@@ -2,21 +2,23 @@
 #include<stdlib.h>
 int main(int argc, char *argv[])
 {
-int tr[2][31], n, i, x;
+struct troj 
+{
+    int pred[33];
+    int po[33];    
+    int n;
+} pas;
+pas.n = 0;
 if (argc != 2)
 {
     puts("Chybny pocet argumentu");
     exit(1);    
 }
-n = atoi(argv[1]);
-if (n > 31)
+pas.n = atoi(argv[1]);
+if (pas.n > 31)
 {
     puts("Moc velky trojuhelnik"); 
     exit(2);   
-}
-for (i = 0; i < n; i++)
-{
-    
 }
 return 0;    
 }
