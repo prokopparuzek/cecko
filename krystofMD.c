9a2a46cc9cfb836b0829872f4ea7c83b43c03056
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2018-01-26 21:28:09 +0100

    jama

--- krystofMD.c
+++ krystofMD.c
@@ -9,19 +9,19 @@ printf("Seed: %d\n",atoi(argv[2]));
 if (argc == 3)
     for (i = 0; i < atoi(argv[1]); i++){
         for (j = 0; j < 3; j++){
-            del = rand() % 100;
+            del = rand() % 1000;
             del = del?del:16;
-            printf("%5d:%2d=\t",rand() % 100000,del);
+            printf("%6d:%3d=\t",rand() % 1000000,del);
         }
         putchar(10);
     }
 else if (argc == 4)
     for (i = 0; i < atoi(argv[1]); i++){
         for (j = 0; j < 3; j++){
-            del = rand() % 100;
+            del = rand() % 1000;
             del = del?del:16;
-            delen = rand() % 100000;
-            printf("%5d:%2d=%5d (%2d)\t",delen,del,delen / del, delen % del);
+            delen = rand() % 1000000;
+            printf("%6d:%3d=%6d (%3d)\t",delen,del,delen / del, delen % del);
         }
         putchar(10);
     }
