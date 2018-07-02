847bbfa49b1b55ab9c904e85b6289245ca362b36
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2018-07-02 17:09:03 +0200

    pascaluvTr

--- cviceni/fibTr.c
+++ cviceni/fibTr.c
@@ -1,8 +1,11 @@
+/*
+funguje pouze u jednocifernych cisel
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int *line(int *a, int *b);
-void print(int *line, int ident);
+int *line(int *a, int *b, int count);
+void print(int *line, int ident, int count);
 
 int main(int argc, char **argv) {
 if (argc != 2) {
@@ -15,8 +18,8 @@ int *swap;
 *line1 = 1;
 int i = atoi(argv[1]);
 while(i-- > 0) {
-    print(line1, i);
-    line(line1, line2);    
+    print(line1, i, atoi(argv[1]));
+    line(line1, line2, atoi(argv[1]));    
     swap = line1;
     line1 = line2; 
     line2 = swap;
@@ -24,22 +27,22 @@ while(i-- > 0) {
 return 0;    
 }
 
-int *line(int *a, int *b) {
-memset(b,0,sizeof(b));
+int *line(int *a, int *b, int count) {
+memset(b,0,count);
 int i;
-for (i = 0; i < sizeof(b)/sizeof(int); i++) {
+for (i = 0; i < count; i++) {
     b[i] = a[i] + (i <= 0 ? 0: a[i - 1]);    
 }
 return b;
 }
 
-void print(int *line, int ident) {
+void print(int *line, int ident, int count) {
 int i;
 for (i = 0; i < ident; i++)
     putchar(' ');
-for (i = 0; i < sizeof(line)/sizeof(int); i++) {
+for (i = 0; i < count;i++) {
     if (line[i] == 0)
-        continue;
+        break;
     else
         printf("%d ",line[i]);    
 }    
