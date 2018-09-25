60c1d758279d3e4d93c197bee24e1f217288ad96
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2018-09-25 21:42:22 +0200

    rim0.1

---
+++ cviceni/prevodyCisel/rim.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+struct table{
+    char character;
+    int number;
+};
+
+struct table map[] = {
+    {'I', 1},
+    {'V', 5},
+    {'X', 10},
+    {'L', 50},
+    {'C', 100},
+    {'D', 500},
+    {'M', 1000}
+};
+
+char *toRoman(int);
+int toArab(char *);
+int getNum(char c);
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        puts("Chybný počet argumentů!");
+        exit(1);
+    }
+    switch (*argv[1]) {
+        case 'r':
+//            toRoman(atoi(argv[2]));
+            break;
+        case 'a':
+            toArab(argv[2]);
+            break;
+        default:
+            puts("Chybný argument\nZadej r|| a!");
+            exit(2);
+            break;
+    }
+}
+
+int toArab(char *rim) {
+    char *s = rim;
+    int arab = 0;
+    while (*s != '\0') {
+        *s = toupper(*s);
+        s++;
+    }
+    s = rim;
+    while (*s != '\0') {
+        if (getNum(*s) < getNum(*(s + 1))?getNum(*(s + 1)):0) {
+            arab += getNum(*(s + 1)) - getNum(*s);
+            s += 2;
+        }
+        else {
+            arab += getNum(*s);
+            s++;
+        }
+    }
+    printf("%d\n", arab);
+}
+
+int getNum(char c){
+    int i;
+    for (i = 0; i < 7; i++) {
+        if (c == map[i].character)
+            return map[i].number;
+        else
+            continue;
+    }
+    puts("Číslo není římské!");
+    exit(3);
+}
--- krystofMD.c
+++ krystofMD.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<time.h>
 #include<stdlib.h>
+#include<math.h>
+#define DELENEC 5
+#define DELITEL 2
 int main(int argc, char *argv[])
 {
 int i, j, del, delen;
@@ -9,18 +12,18 @@ printf("Seed: %d\n",atoi(argv[2]));
 if (argc == 3)
     for (i = 0; i < atoi(argv[1]); i++){
         for (j = 0; j < 3; j++){
-            del = rand() % 1000;
+            del = rand() % (int)pow(10, DELITEL);
             del = del?del:16;
-            printf("%6d:%3d=\t",rand() % 1000000,del);
+            printf("%6d:%3d=\t",rand() % (int)pow(10, DELENEC),del);
         }
         putchar(10);
     }
 else if (argc == 4)
     for (i = 0; i < atoi(argv[1]); i++){
         for (j = 0; j < 3; j++){
-            del = rand() % 1000;
+            del = rand() % (int)pow(10, DELITEL);
             del = del?del:16;
-            delen = rand() % 1000000;
+            delen = rand() % (int)pow(10, DELENEC);
             printf("%6d:%3d=%6d (%3d)\t",delen,del,delen / del, delen % del);
         }
         putchar(10);
