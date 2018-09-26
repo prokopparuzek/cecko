30829dba12e84fbebc47918f5d490979abbfc6a0
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2018-09-26 16:05:10 +0200

    rim0.3

--- cviceni/prevodyCisel/rim.c
+++ cviceni/prevodyCisel/rim.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 struct table{
-    char character;
+    char *character;
     int number;
 };
 
 struct table map[] = {
-    {'I', 1},
-    {'V', 5},
-    {'X', 10},
-    {'L', 50},
-    {'C', 100},
-    {'D', 500},
-    {'M', 1000}
+    {"I", 1},
+    {"IV", 4},
+    {"V", 5},
+    {"IX", 9},
+    {"X", 10},
+    {"XL", 40},
+    {"L", 50},
+    {"XC", 90},
+    {"C", 100},
+    {"CD", 400},
+    {"D", 500},
+    {"CM", 900},
+    {"M", 1000}
 };
 
 char *toRoman(int);
@@ -28,13 +35,13 @@ int main(int argc, char **argv) {
     }
     switch (*argv[1]) {
         case 'r':
-//            toRoman(atoi(argv[2]));
+            toRoman(atoi(argv[2]));
             break;
         case 'a':
             toArab(argv[2]);
             break;
         default:
-            puts("Chybný argument\nZadej r|| a!");
+            puts("Chybný argument\nZadej r || a!");
             exit(2);
             break;
     }
@@ -63,12 +70,29 @@ int toArab(char *rim) {
 
 int getNum(char c){
     int i;
+    char str[2] = {c, '\0'};
     for (i = 0; i < 7; i++) {
-        if (c == map[i].character)
+        if (!strcmp(str, map[i].character))
             return map[i].number;
         else
             continue;
     }
     puts("Číslo není římské!");
     exit(3);
+}
+
+char *toRoman(int ara) {
+    int size = 2, num, i;
+    char *rim = malloc(size);
+    rim[1] = '\0';
+    while (ara) {
+        for (i = 12; ara < map[i].number; i--);
+        if ((strlen(rim) + strlen(map[i].character)) > size - 1) {
+            rim = realloc(rim, size *= 2);
+        }
+        strcat(rim, map[i].character);
+        ara -= map[i].number;
+    }
+    printf("%s\n",rim);
+    free(rim);
 }
