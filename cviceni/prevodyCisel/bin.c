175fefff523054404fc0d9fa535fdc8c4bdeec8d
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-09-23 15:38:46 +0200

    update toBin
    64 bit

--- cviceni/prevodyCisel/bin.c
+++ cviceni/prevodyCisel/bin.c
@@ -1,8 +1,8 @@
 /*
 Převod z desítkové do binární soustavy a zpět
 rozhoduje se podle nazvu programu
-funguje u cisel do velikosti 16 bitu
-cislo zadejte jako argument
+funguje u kladných čísel do velikosti 64 bitu
+číslo zadejte jako argument
  */
 #include<stdio.h>
 #include<stdlib.h>
@@ -10,15 +10,17 @@ cislo zadejte jako argument
 #include<math.h>
 #define BIN "toBin"
 #define DEC "toDec"
-char *toBin(unsigned short d);
-short toDec(char* b);
+
+char *toBin(u_int64_t d);
+u_int64_t toDec(char* b);
+
 int main(int argc, char *argv[])  {
 if (argc == 2)
     if (strcmp(argv[0],BIN) == 0) {
         printf("%s\n",toBin(atoi(argv[1])));    
     }
     else if (strcmp(argv[0],DEC) == 0) {
-        printf("%d\n",toDec(argv[1]));    
+        printf("%lu\n",toDec(argv[1]));    
     }
     else {
         puts("Spatny nazev programu");
@@ -29,11 +31,11 @@ else
 return 0;
 }
 
-char *toBin(unsigned short d) {
-static char b[17];
-char i = 15;
-memset(b,'0',16);
-b[16] = '\0';
+char *toBin(u_int64_t d) {
+static char b[65];
+int i = 63;
+memset(b,'0',64);
+b[64] = '\0';
 while (d != 0) {
     b[i--] = d % 2 + '0';
     d /= 2;
@@ -41,10 +43,10 @@ while (d != 0) {
 return b;
 }
 
-short toDec(char* b) {
-short d = 0;
-char i;
-for (i = 0; i < strlen(b)  && i < 16; i++)
+u_int64_t toDec(char* b) {
+u_int64_t d = 0;
+int i;
+for (i = 0; i < strlen(b)  && i < 64; i++)
     d += pow(2, strlen(b) - 1 - i) * (b[i] - '0');
 return d;
 }
